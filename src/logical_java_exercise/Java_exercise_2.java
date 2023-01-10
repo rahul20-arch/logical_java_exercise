33c3fb17905c661d65286a0862531a6d5b4b7676
rahul20-arch <sivapalanrahul@gmail.com>
2023-01-10 12:02:10 +0530

    palindrome

---
+++ src/logical_java_exercise/Java_exercise_2.java
@@ -0,0 +1,22 @@
+package logical_java_exercise;
+
+public class Java_exercise_2 {
+
+	public static void main(String[] args) {
+
+		String a = "MADAM";		
+		String tem = "";
+		for(int i = 4; i >= 0 ; i--) {
+			tem = tem + a.charAt(i);
+			
+		
+		
+	}
+		System.out.println(tem);
+		
+		if(a.equals(tem)) 
+			System.out.println("Palindrome");
+		else
+			System.out.println("not a palindrome");
+		}
+}

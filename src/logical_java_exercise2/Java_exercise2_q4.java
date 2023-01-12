e077dc50d61801dab91d9d734c987a985b3a9ce2
rahul20-arch <sivapalanrahul@gmail.com>
2023-01-12 11:42:57 +0530

    exercise2_

---
+++ src/logical_java_exercise2/Java_exercise2_q4.java
@@ -0,0 +1,16 @@
+package logical_java_exercise2;
+
+public class Java_exercise2_q4 {
+
+	public static void main(String[] args) {
+		String A = "Rahulsuperman";
+		// A.toLowerCase();
+		//String  C =A.trim();
+		int count = A.replaceAll(" ","").length();
+		System.out.println("Number of alphabets = "+ count);
+		
+		
+		
+	}
+
+}
